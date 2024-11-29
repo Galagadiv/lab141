8ab507bc2ec2a064e06231144bc0457f5216b69e
Galagadiv <sobolia37@gmail.com>
2024-11-29 22:45:02 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Native Camera App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _nativeMessage = 'Hi, mom!';
+  XFile? _image;
+
+  final ImagePicker _picker = ImagePicker();
+  static const platform = MethodChannel('com.example.lab141/native');
+
+  Future<String> getNativeMessage() async {
+    try {
+      final String message = await platform.invokeMethod('getStaticString');
+      _showDialog(message); // Переміщуємо виклик діалогового вікна перед поверненням
+      return message;
+    } on PlatformException catch (e) {
+      return "Failed to get message: '${e.message}'.";
+    }
+  }
+
+  Future<void> _showDialog(String message) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                'https://emojiisland.com/cdn/shop/products/Robot_Emoji_Icon_abe1111a-1293-4668-bdf9-9ceb05cff58e_large.png?v=1571606090',
+                height: 100,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = photo;
+    });
+  }
+
+  Future<void> _delPhoto() async {
+    setState(() {
+      _image = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Лаб14')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          //Text('Native Message: $_nativeMessage'),
+          ElevatedButton(
+            onPressed: getNativeMessage, // Оновлено на виклик getNativeMessage
+            child: const Text('Show String'),
+          ),
+          const SizedBox(height: 20),
+          _image != null
+              ? Image.file(
+            File(_image!.path),
+            fit: BoxFit.cover,
+          )
+              : const Text('No image yet'),
+          ElevatedButton(
+            onPressed: _takePhoto,
+            child: const Text('Take Photo'),
+          ),
+          ElevatedButton(
+            onPressed: _delPhoto,
+            child: const Text('Delete Photo'),
+          ),
+        ],
+      ),
+    );
+  }
+}
